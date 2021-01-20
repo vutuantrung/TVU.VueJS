ded10d42a02066b47df744c32483a4eedb3f46a6
VUTuanTrung <trung.lespace@gmail.com>
2021-01-20 23:12:38 +0100

    [CORE] methods property

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -6,9 +6,16 @@ const app = Vue.createApp({
       vueLink: 'https://vuejs.org/',
     };
   },
+  // methods allows you to define functions
+  methods: {
+    outputGoal: function () {
+      return Math.random() < 0.5 ? 'Learn Vue!' : 'Master Vue';
+    },
+  },
 });
 
 // Apply Vue control into a HTML element
 app.mount('#user-goal');
 
 // 2 core  features of VueJS: data and interpolation {{ param }}
+// In the intnerpolation block, we can add simple javascript EXPRESSION (not STATEMENT - ex: if/else). Ex {{ 1 + 1 }}
