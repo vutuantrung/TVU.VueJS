6da4f6c6bfed8fcee68c5acc69e911645741095c
VUTuanTrung <trung.lespace@gmail.com>
2021-01-20 23:25:53 +0100

    use data in methods

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -2,20 +2,25 @@ const app = Vue.createApp({
   data: function () {
     // always return an object
     return {
-      courseGoal: 'Finish the course and learn Vue!',
+      courseGoalA: 'Finish the course and learn Vue!',
+      courseGoalB: 'Master Vue and build amazing apps!',
       vueLink: 'https://vuejs.org/',
     };
   },
   // methods allows you to define functions
   methods: {
     outputGoal: function () {
-      return Math.random() < 0.5 ? 'Learn Vue!' : 'Master Vue';
+      return Math.random() < 0.5 ? this.courseGoalA : this.courseGoalB;
     },
   },
 });
 
+console.log(app);
+
 // Apply Vue control into a HTML element
 app.mount('#user-goal');
 
-// 2 core  features of VueJS: data and interpolation {{ param }}
-// In the intnerpolation block, we can add simple javascript EXPRESSION (not STATEMENT - ex: if/else). Ex {{ 1 + 1 }}
+// Note: 2 core  features of VueJS: data and interpolation {{ param }}
+// Note: In the intnerpolation block, we can add simple javascript EXPRESSION (not STATEMENT - ex: if/else). Ex {{ 1 + 1 }}
+
+// Note: It basically takes all the data you return in this data object, which you return the data method, and it merges it into a global Vue instance object.
