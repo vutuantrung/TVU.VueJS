00f6f23465d13617ed578a8a49fc98d944dea83f
VUTuanTrung <trung.lespace@gmail.com>
2021-01-21 16:44:40 +0100

    add box select methods

--- basics-10-styling-starting-setup/app.js
+++ basics-10-styling-starting-setup/app.js
@@ -1,5 +1,18 @@
 const app = Vue.createApp({
-
+    data(){
+        return {
+            boxASelected: false,
+            boxBSelected: false,
+            boxCSelected: false,
+        }
+    },
+    methods:{
+        boxSelected(box){
+            this.boxASelected = box === 'A';
+            this.boxBSelected = box === 'B';
+            this.boxCSelected = box === 'C';
+        }
+    }
 });
 
 app.mount('#styling')
