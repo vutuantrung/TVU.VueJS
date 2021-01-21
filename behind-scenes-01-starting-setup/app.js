b09fd93f277f0e0bd9c337f75bc952fd7485104e
VUTuanTrung <trung.lespace@gmail.com>
2021-01-21 21:05:35 +0100

    Vue BTS: Vue use Proxy

--- behind-scenes-01-starting-setup/app.js
+++ behind-scenes-01-starting-setup/app.js
@@ -16,3 +16,7 @@ const app = Vue.createApp({
 });
 
 app.mount('#app');
+
+// NOTE Vue's reactivity: which is one of its core features. Essentially viewed as one important thing. The data you define here is something Vue keeps track of. that it would take this data object here and merge all your properties in here into a global behind the scenes managed object, the same object where your methods are merged into by the way.
+// NOTE IMPORTANT this keyword will point to the global object
+// NOTE but when it comes to the data property, the important and key thing which Vue does is it turns your data object into a reactive data object by essentially wrapping your properties with a JavaScript feature called Proxies.
