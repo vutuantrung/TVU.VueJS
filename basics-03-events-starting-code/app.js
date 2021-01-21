a7edfa49b2cb129448acf1849c541fe0ba277fd2
VUTuanTrung <trung.lespace@gmail.com>
2021-01-21 15:00:37 +0100

    using v-on: passing default parameter for input element

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -2,13 +2,17 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: '',
     };
   },
   methods: {
-    incrementCounter: function (num) {
+    setName(event, lastName) {
+      this.name = lastName + ' ' + event.target.value;
+    },
+    incrementCounter(num) {
       this.counter += num;
     },
-    decrementCounter: function (num) {
+    decrementCounter(num) {
       this.counter -= num;
     },
   },
@@ -18,3 +22,4 @@ app.mount('#events');
 
 // NOTE: use to add eventListener
 // NOTE: we should not add logical expression into HTML section
+// NOTE: there is a behavior in JS, when you add a eventLisener and you then point at a function that should be executed when the event occurs, that function will automatically get one argument which describes the event that occurred, full of information about the event
