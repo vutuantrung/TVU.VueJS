3b82326567b7fa2e428fe40de4347779d2328f7d
VUTuanTrung <trung.lespace@gmail.com>
2021-01-21 14:33:41 +0100

    using v-on

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -4,6 +4,16 @@ const app = Vue.createApp({
       counter: 0,
     };
   },
+  methods: {
+    add: function () {
+      this.counter++;
+    },
+    reduce: function () {
+      this.counter--;
+    },
+  },
 });
 
 app.mount('#events');
+
+// NOTE: use to add eventListener
