cd666dc336eee646ac4e516bfa0529dd29a82808
VUTuanTrung <trung.lespace@gmail.com>
2021-01-21 19:45:21 +0100

    using v-for: remove goal

--- lists-cond-01-starting-setup/app.js
+++ lists-cond-01-starting-setup/app.js
@@ -1,15 +1,18 @@
 const app = Vue.createApp({
   data() {
-    return { 
+    return {
       goals: [],
       enteredGoalValue: '',
     };
   },
-  methods:{
-    addGoal(){
-      this.goals.push(this.enteredGoalValue)
-    }
-  }
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredGoalValue);
+    },
+    removeGoal(index) {
+      this.goals.splice(index, 1);
+    },
+  },
 });
 
 app.mount('#user-goals');
