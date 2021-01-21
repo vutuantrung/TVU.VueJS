0a7157b43a391b380bed4a43e7720b5085139574
VUTuanTrung <trung.lespace@gmail.com>
2021-01-21 20:29:29 +0100

    using v-for: adding key in each element of list

--- lists-cond-01-starting-setup/app.js
+++ lists-cond-01-starting-setup/app.js
@@ -22,3 +22,5 @@ app.mount('#user-goals');
 // IMPORTANT NOTE using v-if: this is not about showing or hiding stuff, it is really about includes/not include the elements in DOM
 // NOTE using v-show: it is not like v-if - including element, v-show just hides the elements (like display: none). We use v-show in case you have an element,which visibility status changes a lot. So, if you have like a button that toggles an element and it's switching between visibility and being hidden all the time
 // IMPORTANT NOTE inside of the v-for controlled element, so between the opening and closing tags of that element, and also on the element itself, we have access to that goal.
+// NOTE when remove an element of a list in DOM, it delete in LIFO way, os the first element still be the old one, it will keep its value and it will be moved to the second one
+// NOTE The key attribute, it is not a default HTML attribute, instead it is an attribute understood and detected by Vue. It's optional, but it is a good idea to add it. You should add it to help Vue tell the different elements apart and make sure that state like such entered value is not getting lost.
