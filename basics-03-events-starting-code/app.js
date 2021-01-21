319ff2758bc740adf31c2eea33294413a4d43a49
VUTuanTrung <trung.lespace@gmail.com>
2021-01-21 15:37:54 +0100

    avoid calling method in HTML file

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -7,6 +7,9 @@ const app = Vue.createApp({
     };
   },
   methods: {
+    outputFullName() {
+      return this.name + ' ' + 'VU TUAN';
+    },
     confirmName(event) {
       this.confirmedName = event.target.value;
     },
@@ -31,3 +34,4 @@ app.mount('#events');
 // NOTE: we should not add logical expression into HTML section
 // NOTE: there is a behavior in JS, when you add a eventLisener and you then point at a function that should be executed when the event occurs, that function will automatically get one argument which describes the event that occurred, full of information about the event
 // NOTE: Vue has "event modifier" to change the event behavior. Ex: v-on:submit.<behavior>. See more at https://vuejs.org/v2/guide/events.html
+// !IMPORTANT : if we add methods in HTML file, VUE will re-executed all of them when there is a change of the page (update a parameter in data), bc Vue do not know what these methods do,
