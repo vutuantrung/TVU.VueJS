5457fabe32bf986280c3cf71040c3fb8dff0e55e
VUTuanTrung <trung.lespace@gmail.com>
2021-01-21 17:48:55 +0100

    using v-if/v-else/v-if-else

--- lists-cond-01-starting-setup/app.js
+++ lists-cond-01-starting-setup/app.js
@@ -15,3 +15,5 @@ const app = Vue.createApp({
 app.mount('#user-goals');
 
 // NOTE using v-if: if the condition is satisfied, the element and its child elements will be rendered
+// NOTE using v-else: The only important thing is that v-else needs to be used on a direct neighbor element of the element which has v-if. You can't have another element in between.
+// IMPORTANT NOTE using v-if: this is not about showing or hiding stuff, it is really about includes/not include the elements in DOM
