3095d8a0d8f70351fa17e2276b518bbeda706802
VUTuanTrung <trung.lespace@gmail.com>
2021-01-22 16:51:11 +0100

    provide/inject: some advices

--- cmp-communication-08-a-potential-problem-starting-setup/src/main.js
+++ cmp-communication-08-a-potential-problem-starting-setup/src/main.js
@@ -18,3 +18,5 @@ app.mount('#app');
 // NOTE Provide and Inject: another feature Vue offers us, and that is, provide and inject, a pattern you can use to provide data in one place and inject it, which means use it, in another place.
 
 // NOTEProvide and Inject: you can only inject what has been provided on a higher-up level. can provide from children -> parent, neighbor -> neighbor
+
+// NOTE we should NOT use provide and inject to replace props and custom events, props and custome events should be default communication mechanism
