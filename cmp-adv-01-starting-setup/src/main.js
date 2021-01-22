afb34c6a04e2265a8bfb0f3206bd0d5d97842856
VUTuanTrung <trung.lespace@gmail.com>
2021-01-22 21:29:00 +0100

    using Local components

--- cmp-adv-01-starting-setup/src/main.js
+++ cmp-adv-01-starting-setup/src/main.js
@@ -1,16 +1,15 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
-import TheHeader from './components/TheHeader.vue';
 import BaseBadge from './components/BaseBadge.vue';
-import BadgeList from './components/BadgeList.vue';
-import UserInfo from './components/UserInfo.vue';
+
 
 const app = createApp(App);
-
-app.component('the-header', TheHeader);
+// Keep this component bc it is used in multiple components
 app.component('base-badge', BaseBadge);
-app.component('badge-list', BadgeList);
-app.component('user-info', UserInfo);
 
 app.mount('#app');
+
+// NOTE if we use app.component(...), we are registering those components as so called Global Components - Components you can use anywhere in your Vue app - i.e any template. And if you're registering them all globally, that in the end means Vue needs to load them all when the application is loaded initially.
+
+// NOTE <the-header /> doesn't work, <TheHeader /> works
