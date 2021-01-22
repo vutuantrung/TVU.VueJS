3d37b1153badc5467d50da2b4b5398924adb42c3
VUTuanTrung <trung.lespace@gmail.com>
2021-01-22 22:15:06 +0100

    using v-slot: create a component and apply slot

--- cmp-adv-01-starting-setup/src/components/UserInfo.vue
+++ cmp-adv-01-starting-setup/src/components/UserInfo.vue
@@ -1,10 +1,14 @@
 <template>
   <section>
-    <div>
-      <h3>{{ fullName }}</h3>
-      <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
-    </div>
-    <p>{{ infoText }}</p>
+    <base-card>
+      <template v-slot:__customHeader>
+        <h3>{{ fullName }}</h3>
+        <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
+      </template>
+      <template v-slot:default>
+        <p>{{ infoText }}</p>
+      </template>
+    </base-card>
   </section>
 </template>
 
@@ -15,14 +19,6 @@ export default {
 </script>
 
 <style scoped>
-section {
-  margin: 2rem auto;
-  max-width: 30rem;
-  border-radius: 12px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  padding: 1rem;
-}
-
 section header {
   display: flex;
   justify-content: space-between;
--- cmp-adv-01-starting-setup/src/main.js
+++ cmp-adv-01-starting-setup/src/main.js
@@ -1,12 +1,14 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
-import BaseBadge from './components/BaseBadge.vue';
+import BaseBadge from './components/BaseBadge';
+import BaseCard from './components/BaseCard';
 
 
 const app = createApp(App);
 // Keep this component bc it is used in multiple components
 app.component('base-badge', BaseBadge);
+app.component('base-card', BaseCard);
 
 app.mount('#app');
 
@@ -17,3 +19,7 @@ app.mount('#app');
 // NOTE <style scoped></style>: used to scope style in that file
 
 // NOTE vue achieves this by giving your elements special attributes
+
+// NOTE Vue has a special syntax for this exact scenario where you want to use your own component as a wrapper around dynamic content. 
+// They allow us to receive HTML content which also may be using Vue features from outside of the component. 
+// Basically just like props but where props are meant to be used for data, which a component needs, slots are meant to be used for HTML code for the template code a component needs.
