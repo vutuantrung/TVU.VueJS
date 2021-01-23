c87cfc788a29d768430b3268e2860e8448c0cc6e
VUTuanTrung <trung.lespace@gmail.com>
2021-01-23 16:37:15 +0100

    get data from firebase using fetch when click button

--- http-01-starting-setup/src/components/survey/UserExperiences.vue
+++ http-01-starting-setup/src/components/survey/UserExperiences.vue
@@ -3,7 +3,9 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadExperiences"
+          >Load Submitted Experiences</base-button
+        >
       </div>
       <ul>
         <survey-result
@@ -21,9 +23,33 @@
 import SurveyResult from './SurveyResult.vue';
 
 export default {
-  props: ['results'],
   components: {
     SurveyResult,
+  },
+  data() {
+    return {
+      results: [],
+    };
+  },
+  methods: {
+    loadExperiences() {
+      fetch(
+        'https://vue-http-demo-60796-default-rtdb.firebaseio.com/surveys.json'
+      )
+        .then(res => res.json())
+        .then(data => {
+          const newResult = [];
+          for (const id in data) {
+            newResult.push({
+              id: id,
+              name: data[id].name,
+              rating: data[id].rating,
+            });
+          }
+
+          this.results = newResult;
+        });
+    },
   },
 };
 </script>
