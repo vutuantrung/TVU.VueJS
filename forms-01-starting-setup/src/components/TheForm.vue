5741ed01951492efcbdb0e651b0d73bed67a1604
VU Tuan Trung <trung.lespace@gmail.com>
2021-01-23 12:00:35 +0100

    working with v-model input text and number
    working with v-model input text and number

--- forms-01-starting-setup/src/components/TheForm.vue
+++ forms-01-starting-setup/src/components/TheForm.vue
@@ -1,12 +1,18 @@
 <template>
-  <form>
+  <form @submit.prevent="submitForm">
     <div class="form-control">
       <label for="user-name">Your Name</label>
-      <input id="user-name" name="user-name" type="text" />
+      <input id="user-name" name="user-name" type="text" v-model="userName" />
     </div>
     <div class="form-control">
       <label for="age">Your Age (Years)</label>
-      <input id="age" name="age" type="number" />
+      <input
+        id="age"
+        name="age"
+        type="number"
+        v-model.number="userAge"
+        ref="ageInput"
+      />
     </div>
     <div class="form-control">
       <label for="referrer">How did you hear about us?</label>
@@ -51,6 +57,26 @@
     </div>
   </form>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      userName: '',
+      userAge: null,
+    };
+  },
+  methods: {
+    submitForm() {
+      console.log(this.userAge);
+      console.log(this.userName);
+
+      this.userName = '';
+      this.userAge = null;
+    },
+  },
+};
+</script>
 
 <style scoped>
 form {
