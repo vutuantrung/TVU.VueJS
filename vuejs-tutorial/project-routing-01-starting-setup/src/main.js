72e8f98cf72ca6ceed8dd11676e2d1b4f81c019e
VUTuanTrung <trung.lespace@gmail.com>
2021-01-24 11:33:48 +0100

    using Vue router to definne 2 url

--- vuejs-tutorial/project-routing-01-starting-setup/src/main.js
+++ vuejs-tutorial/project-routing-01-starting-setup/src/main.js
@@ -1,7 +1,26 @@
 import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
 
-const app = createApp(App)
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/teams',
+      component: TeamsList
+    },
+    {
+      path: '/users',
+      component: UsersList
+    }
+  ] //This will tell Vue that which router will display which component
+});
+
+const app = createApp(App);
+
+app.use(router);
 
 app.mount('#app');
