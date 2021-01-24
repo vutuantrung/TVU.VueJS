d60df7fad27649a5d8cb5c7c1994a781c0186f6b
VUTuanTrung <trung.lespace@gmail.com>
2021-01-24 15:50:13 +0100

    using router: redirect url and "catch all" routes

--- vuejs-tutorial/project-routing-01-starting-setup/src/main.js
+++ vuejs-tutorial/project-routing-01-starting-setup/src/main.js
@@ -10,6 +10,10 @@ const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
+      path: '',
+      redirect: '/teams'
+    },
+    {
       path: '/teams',
       component: TeamsList
     },
@@ -21,6 +25,11 @@ const router = createRouter({
       path: '/teams/:teamId',
       component: TeamMembers,
       props: true
+    },
+    {
+      // This is for '/something' url that client can create, it will redirect to '/teams/
+      path: '/:notFound(.*)',
+      redirect: '/teams'
     }
   ], //This will tell Vue that which router will display which component
   linkActiveClass: 'router-link-active'
