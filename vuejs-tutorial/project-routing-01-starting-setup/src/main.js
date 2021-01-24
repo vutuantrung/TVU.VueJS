3964ef991c2e9912e6c9f4cc6b78ffc4db78137f
VUTuanTrung <trung.lespace@gmail.com>
2021-01-24 16:52:38 +0100

    using router: navigation guards

--- vuejs-tutorial/project-routing-01-starting-setup/src/main.js
+++ vuejs-tutorial/project-routing-01-starting-setup/src/main.js
@@ -51,6 +51,15 @@ const router = createRouter({
   }
 });
 
+router.beforeEach(function(to, from, next) {
+  // if (to.name === 'team-members') {
+  //   next();
+  // } else {
+  //   next({ name: 'team-members', params: { teamId: 't2' } });
+  // }
+  next();
+});
+
 const app = createApp(App);
 
 app.use(router);
