646442af3e73f9c481a299f5ae1185e26368e23a
VUTuanTrung <trung.lespace@gmail.com>
2021-01-24 22:41:14 +0100

    implement vuex feature MUTATIONS to handle common methods

--- vuejs-tutorial/project-vuex-01-starting-setup/src/App.vue
+++ vuejs-tutorial/project-vuex-01-starting-setup/src/App.vue
@@ -2,21 +2,24 @@
   <base-container title="Vuex">
     <TheCountner />
     <button @click="incrementCounter">Add 1</button>
+    <ChangeCounter />
   </base-container>
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
 import TheCountner from './components/TheCounter.vue';
+import ChangeCounter from './components/ChangeCounter.vue';
 
 export default {
   components: {
     BaseContainer,
     TheCountner,
+    ChangeCounter,
   },
   methods: {
     incrementCounter() {
-      this.$store.state.counter++;
+      this.$store.commit('increment');
     },
   },
 };
---
+++ vuejs-tutorial/project-vuex-01-starting-setup/src/components/ChangeCounter.vue
@@ -0,0 +1,12 @@
+<template lang="">
+<button @click="incrementCounter">Add 1</button>
+</template>
+<script>
+export default {
+  methods: {
+    incrementCounter() {
+      this.$store.commit('increment');
+    },
+  },
+};
+</script>
