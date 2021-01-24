6596cd528c7a53330eeb3dfc6311fc9a3ac9be54
VUTuanTrung <trung.lespace@gmail.com>
2021-01-24 17:35:46 +0100

    using router: using beforeRouteLeave to make sure no unsaved changes

--- vuejs-tutorial/project-routing-01-starting-setup/src/components/users/UsersList.vue
+++ vuejs-tutorial/project-routing-01-starting-setup/src/components/users/UsersList.vue
@@ -1,5 +1,6 @@
 <template>
   <button @click="confirmInput">Confirm</button>
+  <button @click="saveChanges">Save changes</button>
   <ul>
     <user-item
       v-for="user in users"
@@ -17,14 +18,38 @@ export default {
   components: {
     UserItem,
   },
+
   inject: ['users'],
+
   methods: {
     confirmInput() {
       // do somthing(Ex: send http request...)
 
       this.$router.push('/teams'); // push url to history, looing for more in docs (push(), back(), forward())
     },
+    saveChanges() {
+      this.changesSaved = true;
+    },
   },
+
+  data() {
+    return {
+      changesSaved: false,
+    };
+  },
+
+  beforeRouteLeave(to, from, next) {
+    console.log('UsersList beforeRouteLeave');
+
+    // Make sure that data is saved
+    if (this.changesSaved) {
+      next();
+    } else {
+      const leave = confirm('You have unsaved changes? Want to leave ?');
+      next(leave);
+    }
+  },
+
   beforeRouteEnter(to, from, next) {
     console.log('UsersList beforeRouteEnter');
     next();
