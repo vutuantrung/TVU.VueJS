2039f9eb42eddc38e720aa5f7a14efd80f6b948a
VUTuanTrung <trung.lespace@gmail.com>
2021-01-25 13:39:46 +0100

    preparing vuex for authentication section

--- vuejs-tutorial/project-auth-01-starting-project/src/store/index.js
+++ vuejs-tutorial/project-auth-01-starting-project/src/store/index.js
@@ -1,22 +1,14 @@
 import { createStore } from 'vuex';
 
-import coachesModule from './modules/coaches/index.js';
-import requestsModule from './modules/requests/index.js';
+import coachesModule from './modules/coaches/index';
+import requestsModule from './modules/requests/index';
+import authModule from './modules/auth/index';
 
 const store = createStore({
   modules: {
     coaches: coachesModule,
-    requests: requestsModule
-  },
-  state() {
-    return {
-      userId: 'c3'
-    };
-  },
-  getters: {
-    userId(state) {
-      return state.userId;
-    }
+    requests: requestsModule,
+    auth: authModule
   }
 });
 
