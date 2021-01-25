423d3470ea82b9f31444b29aef848369d6a42d41
VUTuanTrung <trung.lespace@gmail.com>
2021-01-25 13:33:07 +0100

    adding authentication page (login and signup)

--- vuejs-tutorial/project-auth-01-starting-project/src/store/modules/requests/actions.js
+++ vuejs-tutorial/project-auth-01-starting-project/src/store/modules/requests/actions.js
@@ -5,7 +5,7 @@ export default {
       message: payload.message
     };
     const response = await fetch(
-      `https://vue-http-demo-85e9e.firebaseio.com/requests/${payload.coachId}.json`,
+      `https://vue-http-demo-60796-default-rtdb.firebaseio.com/requests/${payload.coachId}.json`,
       {
         method: 'POST',
         body: JSON.stringify(newRequest)
@@ -29,7 +29,7 @@ export default {
   async fetchRequests(context) {
     const coachId = context.rootGetters.userId;
     const response = await fetch(
-      `https://vue-http-demo-85e9e.firebaseio.com/requests/${coachId}.json`
+      `https://vue-http-demo-60796-default-rtdb.firebaseio.com//requests/${coachId}.json`
     );
     const responseData = await response.json();
 
