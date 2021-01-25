17a69699d8bf20a5ccfa3109e975cd581ff1c391
VUTuanTrung <trung.lespace@gmail.com>
2021-01-25 14:08:59 +0100

    UX: loading Spinner and Error handling

--- vuejs-tutorial/project-auth-01-starting-project/src/pages/auth/UserAuth.vue
+++ vuejs-tutorial/project-auth-01-starting-project/src/pages/auth/UserAuth.vue
@@ -1,24 +1,37 @@
 <template>
-  <base-card>
-    <form @submit.prevent="submitForm">
-      <div class="form-control">
-        <label for="email">E-mail</label>
-        <input type="email" id="email" v-model.trim="email" />
-      </div>
-      <div class="form-control">
-        <label for="password">Password</label>
-        <input type="password" id="password" v-model.trim="password" />
-      </div>
-      <p v-if="!formIsValid">
-        Please enter a valid email and password (must be at least 6 characters
-        long).
-      </p>
-      <base-button>{{ submitButtonCaption }}</base-button>
-      <base-button type="button" mode="flat" @click="switchAuthMode">{{
-        switchModeButtonCaption
-      }}</base-button>
-    </form>
-  </base-card>
+  <div>
+    <base-dialog
+      :show="!!error"
+      title="An error occurred."
+      @close="handleError"
+    >
+      <p>{{ error }}</p>
+    </base-dialog>
+    <base-dialog :show="isLoading" title="Authenticating..." fixed>
+      <p>Authenticating...</p>
+      <base-spinner></base-spinner>
+    </base-dialog>
+    <base-card>
+      <form @submit.prevent="submitForm">
+        <div class="form-control">
+          <label for="email">E-mail</label>
+          <input type="email" id="email" v-model.trim="email" />
+        </div>
+        <div class="form-control">
+          <label for="password">Password</label>
+          <input type="password" id="password" v-model.trim="password" />
+        </div>
+        <p v-if="!formIsValid">
+          Please enter a valid email and password (must be at least 6 characters
+          long).
+        </p>
+        <base-button>{{ submitButtonCaption }}</base-button>
+        <base-button type="button" mode="flat" @click="switchAuthMode">{{
+          switchModeButtonCaption
+        }}</base-button>
+      </form>
+    </base-card>
+  </div>
 </template>
 
 <script>
@@ -29,6 +42,8 @@ export default {
       password: '',
       formIsValid: true,
       mode: 'login',
+      isLoading: false,
+      error: null,
     };
   },
   computed: {
@@ -40,35 +55,43 @@ export default {
     },
   },
   methods: {
-    submitForm() {
+    async submitForm() {
       this.formIsValid = true;
       if (
         this.email === '' ||
         !this.email.includes('@') ||
         this.password.length < 6
       ) {
-        console.log(this.email);
-        console.log(this.password);
-
         this.formIsValid = false;
         return;
       }
 
+      this.isLoading = true;
       // send Http request
-      if (this.mode === 'login') {
-        // ...
-      } else {
-        console.log('sign up here');
+      try {
+        if (this.mode === 'login') {
+          // ...
+        } else {
+          console.log('sign up here');
 
-        this.$store.dispatch('signup', {
-          email: this.email,
-          password: this.password,
-        });
+          await this.$store.dispatch('signup', {
+            email: this.email,
+            password: this.password,
+          });
+        }
+      } catch (err) {
+        this.error =
+          err.message || 'Failed to authenticate. Check you login data...';
       }
+
+      this.isLoading = false;
     },
     switchAuthMode() {
       this.mode = this.mode === 'signup' ? 'login' : 'signup';
       console.log(this.mode);
+    },
+    handleError() {
+      this.error = null;
     },
   },
 };
