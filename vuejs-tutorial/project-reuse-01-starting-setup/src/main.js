f29618e2e4bf14c526cea6f82e1a25ef341dc6ad
VUTuanTrung <trung.lespace@gmail.com>
2021-01-26 13:22:17 +0100

    using global mixins

--- vuejs-tutorial/project-reuse-01-starting-setup/src/main.js
+++ vuejs-tutorial/project-reuse-01-starting-setup/src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
+import loggerMixins from './mixins/logger';
 
 const app = createApp(App);
+
+app.mixin(loggerMixins);
 
 app.mount('#app');
